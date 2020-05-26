33c5f45ebb6a1813f4de97bdda0a3e3155044ed0
Prathamesh More <pprathameshmore@outlook.com>
2020-05-26 19:45:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-//const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
